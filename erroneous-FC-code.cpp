9f63262dd8cd493241ff67de341dd5735d693fbb
Tara Meazell <tarameazell@gmail.com>
2024-02-14 15:56:27 -0600

    added more todo to FC code and filled fc lists

--- erroneous-FC-code.cpp
+++ erroneous-FC-code.cpp
@@ -4,12 +4,12 @@
 using namespace std;
 
 int flag;
-string cndvar[10];
-string varlt[10], clvarlt[40]; // Variable lists
+string cndvar[10]; //condition variable
+string varlt[10], clvarlt[40]; // Variable list and clause variable list //TODO change these to appropriate lengths
 string c, vp, v; // Condition variables
-string fedint, interest, stock, dollar, fedmon;
-string po, qu; // Conclusion variables
-int instlt[10]; // Instantiated list
+string fedint, interest, stock, dollar, fedmon; //TODO use the variable from FC lists.txt
+string po, qu; // Conclusion variables TODO change these to conclusions from FC lists
+int instlt[10]; // Instantiated list (for instantiated variables)
 //TODO: remove unnecesarily global variables such as loop counters
 int f, i, j, k, s, fp, bp, gr, sn, cn; // Pointers and counters 
 
@@ -20,8 +20,8 @@ void instantiate();
 
 int main() {
     // Initialization section
-    fp = 1;
-    bp = 1;
+    fp = 1; //front pointer
+    bp = 1; //back pointer
 
     for (i = 1; i < 41; i++)
         clvarlt[i] = "";
@@ -33,13 +33,14 @@ int main() {
     }
 
     // Enter variables in the IF part
+    //TODO change this to match variable list from FC lists.txt
     varlt[1] = "DO";
     varlt[2] = "FT";
     varlt[3] = "FM";
     varlt[4] = "IN";
     varlt[5] = "ST";
 
-    cout << "*** VARIABLE LIST ***" << endl;
+    cout << "*** VARIABLE LIST ***" << endl; //we only have one variable. It seems like we need to get the variable from the BC function?
     for (i = 1; i < 11; i++)
         cout << "ENTER VARIABLE " << i << " " << varlt[i] << endl;
 
@@ -47,6 +48,7 @@ int main() {
     getchar();
 
     // Enter variables as they appear in the IF clauses
+    //TODO make this match the clause variable list from FC lists.txt
     clvarlt[1] = "IN";
     clvarlt[5] = "IN";
     clvarlt[9] = "DO";
@@ -68,7 +70,7 @@ int main() {
     }
 
     // Inference section
-    cout << "ENTER CONDITION VARIABLE? ";
+    cout << "ENTER CONDITION VARIABLE? "; //basically asking the user what they want to find. In this case, we are looking for PREVENTION
     getline(cin, c);
 
     // Place condition variable c on the condition variable queue cndvar
@@ -76,18 +78,18 @@ int main() {
     bp++;
 
     // Set the condition variable pointer
-    sn = 1;
-    cn = 1;
+    sn = 1; //statement number
+    cn = 1; //clause number
 
-    // Find the next statement number containing the condition variable
+    // Find the next statement number containing the condition variable (ie )
     // which is in front of the queue (cndvar), this statement number
     // is located in the clause variable list (clvarlt)
-    f = 1;
+    f = 1; //f is an iterator
 
     while (sn != 0) {
-        search();
+        search(); //b496 //search() searches the clause variable list for our condition ATTACK
 
-        cn = 1;
+        cn = 1; // start at the first clause
         if (sn != 0) {
             i = 4 * (sn - 1) + cn;
 
@@ -99,7 +101,7 @@ int main() {
             /* no more clauses - check IF part of statement */
             s = 0;
 
-            switch (sn) {
+            switch (sn) { //TODO change this
                 case 1: if (interest == "FALL") s = 1; break;
                 case 2: if (interest == "RISE") s = 1; break;
                 case 3: if (dollar == "FALL") s = 1; break;
@@ -110,7 +112,7 @@ int main() {
             /* see if the THEN part should be inovked, i.e., s=1 */
             if (s != 1) {
                 f = sn + 1;
-                continue;
+                continue; //goto b496
             }
 
             switch (sn) {
@@ -122,22 +124,31 @@ int main() {
                 case 6: po = "YES"; cout << "PO=YES" << endl; break;
             }
 
-            f = sn + 1;
+            f = sn + 1; 
+            continue; //goto b496
         }
 
-        // No more clauses in the clause variable list (clvarlt) containing the variable
-        // in front of the conclusion queue (cndvar(fp))
-        // Remove front variable (cndvar(fp)) and replace it by the next variable (cndvar(fp+1))
+        /* no more clauses in the clause variable list (clvarlt)
+        containing the variable in front of the queue (cndvar(fp))
+        then remove front variable (cndvar(fp)) and replace it by
+        the next variable (cndvar(fp+1)). If no more variables are
+        at the front of the queue, stop. */
+        fp++; // next queue variable
         if (fp < bp) {
-            fp++;
+            //check out the condition variable 
             f = 1;
+            continue; //goto b496
         }
+        //no more conclusion variables on queue
+        break;
     }
 
     return 0;
 }
 
 // Function to instantiate a variable (v) if it isn't already instantiated
+//The instantiate indication (instlt) is a 0 if not, a 1 if it is.
+//The vriable list (varlt) contains the variable (v)
 void check_instantiation() {
     i = 1;
 
@@ -147,7 +158,7 @@ void check_instantiation() {
     if (instlt[i] != 1) {
         instlt[i] = 1;
 
-        switch (i) {
+        switch (i) { //TODO change this to ask what type of attack was identified
             case 1: cout << "RISE OR FALL FOR DO? "; getline(cin, dollar); break;
             case 2: cout << "RISE OR FALL FOR FT? "; getline(cin, fedint); break;
             case 3: cout << "ADD OR SUBTRACT FOR FM? "; getline(cin, fedmon); break;
@@ -158,11 +169,11 @@ void check_instantiation() {
 }
 
 // Function to search the clause variable list for a variable (clvarlt)
-int search() {
+void search() {
     flag = 0;
-    sn = f;
+    sn = f; //set sn to f
 
-    while (flag == 0 && sn <= 10) {
+    while (flag == 0 && sn <= 10) { //TODO change number of iterations to match our # of statements
         cn = 1;
         k = (sn - 1) * 4 + cn;
 
@@ -171,7 +182,7 @@ int search() {
             k = (sn - 1) * 4 + cn;
         }
 
-        if (clvarlt[k] == cndvar[fp])
+        if (clvarlt[k] == cndvar[fp]) //if we find the condition variable
             flag = 1;
 
         if (flag == 0)
@@ -179,16 +190,15 @@ int search() {
     }
 
     if (flag == 0)
-        sn = 0;
+        sn = 0; 
 
-    return sn;
 }
 
 // Function to instantiate a variable (v) and place it on the back of the queue (cndvar[bp])
 void instantiate() {
     i = 1;
 
-    while (v != varlt[i] && i <= 10)
+    while (v != varlt[i] && i <= 10) //TODO change the number of iterations
         i++;
 
     instlt[i] = 1;
